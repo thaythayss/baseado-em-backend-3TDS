c6e7c583088a6ca0bb501e2a8dea6e3ec0ee4381
thaythayss <thays.castro.andrade@escola.pr.gov.br>
2026-05-04 11:46:24 +0000

    versao1

---
+++ cardapio.js
@@ -0,0 +1,12 @@
+const cardapio = [
+    {nome: "X-Burguer", preco: 18.90, categoria:"lanches"},
+    {nome: "Batata-frita", preco: 9.50, categoria:"acompanhamento"},
+    {nome: "Suco de Laranja", preco: 7.00, categoria: "bebida"},
+    {nome: "Milkshake", preco: 12.00, categoria: "sobremesa"}
+];
+
+console.log(cardapio);
+
+const pedido = [];
+
+console.log(cadarpio[0].nome);
