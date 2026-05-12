144569530b0877409324bc4b578bfe4327471aac
thaythayss <thays.castro.andrade@escola.pr.gov.br>
2026-05-12 11:52:54 +0000

    versao1

---
+++ desafio.js
@@ -0,0 +1,8 @@
+const loja = "SuperMercado";
+const produto = "Vassoura";
+const categoria = "limpeza";
+let preco = 20.00;
+let desconto = 0.10;
+let precoFinal = preco - (preco * desconto);
+
+console.log(``)
