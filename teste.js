b5181596daee414a331ffac1c0f83ae83da447ee
thaythayss <thays.castro.andrade@escola.pr.gov.br>
2026-05-12 11:40:41 +0000

    versao1

---
+++ teste.js
@@ -0,0 +1,7 @@
+const destino = "Foz do Iguaçu";
+const origem = "Curitiba";
+const nomeViajante = "Luiza";
+let duracao = 8;
+let transporte = "carro";
+
+console.log(`${nomeViajante} viajará de ${origem} para ${destino} por ${duracao}h de ${transporte}.`);
