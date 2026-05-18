e76293f07168270c856b158300d219b388636022
thaythayss <thays.castro.andrade@escola.pr.gov.br>
2026-05-18 11:38:25 +0000

    versao2

--- desafio.js
+++ desafio.js
@@ -1,8 +1,19 @@
 const loja = "SuperMercado";
 const produto = "Vassoura";
-const categoria = "limpeza";
+const categoria = "Limpeza";
 let preco = 20.00;
 let desconto = 0.10;
 let precoFinal = preco - (preco * desconto);
 
-console.log(``)
+let desconto2 = 0.20;
+let precoFinal2 = preco - (preco * desconto2);
+
+let compras = 500;
+let final = compras - (compras * desconto2);
+
+let economia = (compras - final)
+
+console.log(`Venha conferir nosso incrível produto: ${produto}, que foi de ${preco} para ${precoFinal}! É a melhor ferramenta para sua ${categoria}.`)
+console.log(`${desconto} * ${preco} = ${precoFinal}`)
+
+console.log(`A partir de R$500,00, você tem 20% de desconto! Ao final, você vai pagar: R$${final}, economizando R$${economia}`)
