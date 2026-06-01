45617e613e3db6fb3e3b2c296e45c6c1abc9b6cf
thaythayss <thays.castro.andrade@escola.pr.gov.br>
2026-06-01 11:52:35 +0000

    versao1

---
+++ ficha.js
@@ -0,0 +1,11 @@
+//Criar um objeto que represente o mundo real
+const animal = {
+    nome: "Thor", //string
+    especie: "Cachorro", //string
+    idade: 4, //number
+    tutor: true, //boolean
+    vacinado: false // boolean
+};
+
+console.log(animal.nome);
+console.log(animal.tutor);
